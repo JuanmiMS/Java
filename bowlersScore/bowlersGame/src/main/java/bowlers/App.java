f61ccc1fc1ced1da34f0d6f35b31173712e35800
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-01-19 11:33:55 +0100

    Updated bowlersScore

---
+++ bowlersScore/bowlersGame/src/main/java/bowlers/App.java
@@ -0,0 +1,13 @@
+package bowlers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
