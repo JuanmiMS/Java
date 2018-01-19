fd07005c112864c661c4f33954c6564f6073eda2
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-01-19 10:45:45 +0100

    kata arrayPlusArray finished

--- CodeWars/KatasCodeWars/src/main/java/jar/SimpleExercices.java
+++ CodeWars/KatasCodeWars/src/main/java/jar/SimpleExercices.java
@@ -1,6 +1,9 @@
 package jar;
 public class SimpleExercices 
 {
+    public static void main(String [] args){
+        arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6});
+    }
 
     //https://www.codewars.com/kata/54ff0d1f355cfd20e60001fc/train/java
     public static int factorial(int n) {
@@ -12,6 +15,23 @@ public class SimpleExercices
         return sum;
         
     }
+    //https://www.codewars.com/kata/array-plus-array/train/java
+    public static int arrayPlusArray(int[] arr1, int[] arr2) {
+        
+        int totalSum = 0;
+
+        //Array 1
+        for(int i=0; i< arr1.length; i++){
+            totalSum += arr1[i];
+        }
+
+        //Array 2
+        for(int i=0; i< arr2.length; i++){
+            totalSum += arr2[i];
+        }
+        
+        return totalSum;
+      }
 
     
 }
--- CodeWars/KatasCodeWars/src/test/java/jar/SimpleExercicesTest.java
+++ CodeWars/KatasCodeWars/src/test/java/jar/SimpleExercicesTest.java
@@ -15,10 +15,17 @@ public class SimpleExercicesTest
 extends TestCase
 {
     @Test
-    public void testFactorial()
-    {
+    public void testFactorial(){
         assertTrue("Falla test 1", 1 == SimpleExercices.factorial(0));
         assertTrue("Falla test 2", 6 == SimpleExercices.factorial(3));
         assertTrue("Falla test 3", 120 == SimpleExercices.factorial(5));
+    }
+
+    @Test
+    public void testArrayPlusArray(){
+        assertEquals(21, SimpleExercices.arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
+        assertEquals(-21, SimpleExercices.arrayPlusArray(new int[]{-1,-2,-3}, new int[]{-4,-5,-6}));
+        assertEquals(15, SimpleExercices.arrayPlusArray(new int[]{0,0,0}, new int[]{4,5,6}));
+        assertEquals(2100, SimpleExercices.arrayPlusArray(new int[]{100,200,300}, new int[]{400,500,600}));
     }
 }
