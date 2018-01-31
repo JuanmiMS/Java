adc43a2a03bbbff2bcdc39090e2d108118cbb27b
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-01-31 20:33:24 +0100

    Todos los casos test pasados

--- comprobarDNI/src/test/java/org/mvpigs/comprobardni/testCheckDNI.java
+++ comprobarDNI/src/test/java/org/mvpigs/comprobardni/testCheckDNI.java
@@ -6,10 +6,14 @@ import junit.framework.TestCase;
 public class testCheckDNI extends TestCase{
     @Test
     public void testCheckLetter(){
-        
+        //devuelve 'o' si da error
         checkDNI midni = new checkDNI("41540269");
-        checkDNI midniError = new checkDNI("44r");
+
         assertEquals('S', midni.getLetra());
-        assertEquals('o', midniError.getLetra());
+        assertEquals('G', new checkDNI("47455398").getLetra());
+        assertEquals('J', new checkDNI("49878779").getLetra());
+        assertEquals('F', new checkDNI("26986850").getLetra());
+        assertEquals('o', new checkDNI("asdasf").getLetra());
+        assertEquals('M', new checkDNI("51871997").getLetra());
     }
 }
