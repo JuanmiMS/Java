6c60b52e56fbd7192b590866a019d43c95c97320
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-01-31 19:35:52 +0100

    getters y setter de comprobarLetra

--- comprobarDNI/src/main/java/org/mvpigs/comprobarDNI/mainDNI.java
+++ comprobarDNI/src/main/java/org/mvpigs/comprobarDNI/mainDNI.java
@@ -1,9 +1,10 @@
 package org.mvpigs.comprobarDNI;
 
-public class App 
-{
+public class mainDNI{
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Wofdfsrld!" );
     }
+
+    
 }
