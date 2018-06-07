75a6b5446fa426c022f207f0abc2e8c4084b79d2
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-06-07 17:25:55 +0200

    test_interface_procesador pasado

--- ExamenJava/src/test/java/org/mvpigs/TestPedidos.java
+++ ExamenJava/src/test/java/org/mvpigs/TestPedidos.java
@@ -133,20 +133,20 @@ public class TestPedidos {
      * tipo de pedido
      */
 
-//    @Test
-//    public void test_interface_procesador() {
-//
-//        Procesador correos = new Oficina();
-//        TratamientoPedido pedidoInt = new TratamientoPedidoInternacional(
-//                                            new PedidoInternacional("Comarca", 100));
-//        assertTrue(correos.procesa(pedidoInt));
-//
-//        TratamientoPedido pedidoConPeligro = new TratamientoPedidoPeligroso(
-//                                                 new PedidoPeligrosoOrden(
-//                                                        "Cima de los vientos",
-//                                                        "no limpiarse las uñas con este puñal"));
-//        assertTrue(correos.procesa(pedidoConPeligro));
-//    }
+    @Test
+    public void test_interface_procesador() {
+
+        Procesador correos = new Oficina();
+        TratamientoPedido pedidoInt = new TratamientoPedidoInternacional(
+                                            new PedidoInternacional("Comarca", 100));
+        assertTrue(correos.procesa(pedidoInt));
+
+        TratamientoPedido pedidoConPeligro = new TratamientoPedidoPeligroso(
+                                                 new PedidoPeligrosoOrden(
+                                                        "Cima de los vientos",
+                                                        "no limpiarse las uñas con este puñal"));
+        assertTrue(correos.procesa(pedidoConPeligro));
+    }
 
     /**
      * La oficina puede enviar un mensaje que informe del
