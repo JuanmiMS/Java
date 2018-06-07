313b06d98abbf6db3ce16bb1a96d9ed3dbb592ed
Juan Miguel <juanmi.marisanchez@gmail.com>
2018-06-07 17:52:22 +0200

    examen finalizado

--- ExamenJava/src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoMultiple.java
+++ ExamenJava/src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoMultiple.java
@@ -1,4 +1,45 @@
 package org.mvpigs.commandpattern.tratamientos;
 
-public class TratamientoPedidoMultiple {
+import org.mvpigs.commandpattern.interfaces.Pedido;
+
+import java.util.ArrayList;
+
+
+public class TratamientoPedidoMultiple  {
+
+    ArrayList<Pedido> pedidos = new ArrayList();
+    private int totalBultos;
+    private int totalPeso;
+
+
+    public TratamientoPedidoMultiple(ArrayList pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    public void calcularTotalBultos(){
+        for(Pedido pedido : pedidos){
+            totalBultos++;
+        }
+    }
+    
+    public void calcularPesoTotal(){
+        for(Pedido pedido : pedidos){
+            this.totalPeso+=pedido.peso();
+        }
+    }
+
+    public double getNumBultos() {
+        return this.totalBultos;
+    }
+
+    public double getPesoTotal() {
+        return this.totalPeso;
+    }
+
+    public boolean tratar() {
+        if(this.getPesoTotal() > 0 && this.getNumBultos() == pedidos.size()){
+            return true;
+        }
+        return false;
+    }
 }
--- ExamenJava/src/test/java/org/mvpigs/TestPedidos.java
+++ ExamenJava/src/test/java/org/mvpigs/TestPedidos.java
@@ -18,6 +18,8 @@ import org.mvpigs.commandpattern.tratamientos.TratamientoPedidoInternacional;
 import org.mvpigs.commandpattern.tratamientos.TratamientoPedidoMultiple;
 import org.mvpigs.commandpattern.tratamientos.TratamientoPedidoPeligroso;
 
+import java.util.ArrayList;
+
 /**
  * NO PUEDES MODIFICAR EL CODIGO DE LOS CASOS TEST
  */
@@ -177,61 +179,68 @@ public class TestPedidos {
                 pedidoConPeligro));
 
     }
-//
-//    /**
-//     * Crea una clase TratamientoPedidoMultiple que permita tratar
-//     * pedidos multiples.
-//     *
-//     * La clase permite tratar el pedido multiple si
-//     * el peso total de los pedidos es mayor que 0
-//     * y
-//     * el numero de bultos coincide con el numero de
-//     * pedidos individuales que forman el pedido multiple.
-//     *
-//     * Crea las clases necesarias que se requieren en los casos test
-//     * respetando los constructores que se exigen.
-//     */
-//
-//    @Test
-//    public void test_tratamiento_pedido_multiple_tratar() {
-//
-//        /**
-//         * Crea una colección de tres pedidos nacionales,
-//         * a "Gondor", "Minas Tirith", "Rohan"
-//         * con un peso de 10 cada uno.
-//         *
-//         * Pasasela a TratamientoPedidosMultiple en su constructor.
-//         */
-//
-//        // Coleccion pedidos
-//        assertTrue(pedidos.size() == 3);
-//
-//        TratamientoPedidoMultiple pedidosMult = new TratamientoPedidoMultiple(pedidos);
-//        assertNotNull(pedidosMult);
-//
-//        /**
-//         * Completa los metodos del pedido multiple.
-//         * Se valorara el uso de streams.
-//         *
-//         * calcularTotalBultos
-//         * @param   void
-//         * @return  void
-//         *
-//         * calcularPesoTotal
-//         * @param   void
-//         * @return  void
-//         *
-//         */
-//
-//        pedidosMult.calcularTotalBultos();
-//        assertEquals(3, pedidosMult.getNumBultos(), 0);
-//
-//        pedidosMult.calcularPesoTotal();
-//        assertEquals(30, pedidosMult.getPesoTotal(), 0);
-//
-//        /**
-//         * Trata el pedido multiple.
-//         */
-//        assertTrue(pedidosMult.tratar());
-//    }
+
+    /**
+     * Crea una clase TratamientoPedidoMultiple que permita tratar
+     * pedidos multiples.
+     *
+     * La clase permite tratar el pedido multiple si
+     * el peso total de los pedidos es mayor que 0
+     * y
+     * el numero de bultos coincide con el numero de
+     * pedidos individuales que forman el pedido multiple.
+     *
+     * Crea las clases necesarias que se requieren en los casos test
+     * respetando los constructores que se exigen.
+     */
+
+    @Test
+    public void test_tratamiento_pedido_multiple_tratar() {
+
+        /**
+         * Crea una colección de tres pedidos nacionales,
+         * a "Gondor", "Minas Tirith", "Rohan"
+         * con un peso de 10 cada uno.
+         *
+         * Pasasela a TratamientoPedidosMultiple en su constructor.
+         */
+
+
+        ArrayList pedidos = new ArrayList();
+        pedidos.add(new PedidoNacional("Gondor", 10));
+        pedidos.add(new PedidoNacional("Minas Tirith", 10));
+        pedidos.add(new PedidoNacional("Rohan", 10));
+
+        // Coleccion pedidos
+        assertTrue(pedidos.size() == 3);
+
+        TratamientoPedidoMultiple pedidosMult = new TratamientoPedidoMultiple(pedidos);
+        assertNotNull(pedidosMult);
+
+        /**
+         * Completa los metodos del pedido multiple.
+         * Se valorara el uso de streams.
+         *
+         * calcularTotalBultos
+         * @param   void
+         * @return  void
+         *
+         * calcularPesoTotal
+         * @param   void
+         * @return  void
+         *
+         */
+
+
+        pedidosMult.calcularTotalBultos();
+        assertEquals(3, pedidosMult.getNumBultos(), 0);
+
+        pedidosMult.calcularPesoTotal();
+        assertEquals(30, pedidosMult.getPesoTotal(), 0);
+
+        /**
+         * Trata el pedido multiple.
+         */
+        assertTrue(pedidosMult.tratar());
+    }
 }
